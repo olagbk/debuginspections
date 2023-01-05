a3d90c064d540056d4b34121fe47196335366cb0
Aleksandra <aleksandra.gabka@datafeedwatch.com>
2023-01-05 16:46:17 +0100

    add example code to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `{{method()}}`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'debuginspections';
+  @Input() title = 'debuginspections';
+
+  method(): number {
+    let zero = 0;
+    let one = zero + 1;
+    return one;
+  }
 }
